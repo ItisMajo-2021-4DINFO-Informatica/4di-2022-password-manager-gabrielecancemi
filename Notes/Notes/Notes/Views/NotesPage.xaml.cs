8e48dcafe11e49d4a275ed3b38fc100eb3a7bf1e
Gabriele Cancemi <gabrielecancemi2004@gmail.com>
2022-02-03 12:54:59 +0100

    Risolto il problema del titolo

--- Notes/Notes/Notes/Views/NotesPage.xaml.cs
+++ Notes/Notes/Notes/Views/NotesPage.xaml.cs
@@ -24,12 +24,18 @@ namespace Notes.Views
             var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.txt");
             foreach (var filename in files)
             {
+                string allText = File.ReadAllText(filename);
+                string[] campi = allText.Split('§');
                 notes.Add(new Note
                 {
                     Filename = filename,
-                    ServiceName = File.ReadAllText(filename),
+                    ServiceName = campi[0],
+                    Username = campi[1],
+                    Password = campi[2],
+                    URL = campi[3],
                     Date = File.GetCreationTime(filename)
                 });
+                
             }
 
             // Set the data source for the CollectionView to a
